a771e4f55c5931fe1b45e490dae86a91bb7b300a
nandu83 <113989164+nandu83@users.noreply.github.com>
2022-12-01 09:03:45 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -2,4 +2,6 @@ This file contains DB related info java file
 Addinng this line by bovivek user
 30-nov-22 - Adding new changes
 
-Updating new line for demo
+Updating new line for demo - Fetch
+
+This line is for pull demo
